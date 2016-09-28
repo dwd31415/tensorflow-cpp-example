83e44ba61acf2201709bd79a8195c379fc68f845
Adrian Dawid <adriandwd@gmail.com>
2016-09-28 19:17:31 +0200

    Correct types

--- example.cc
+++ example.cc
@@ -15,12 +15,12 @@ public:
   void Compute(OpKernelContext* context) override {
     // Get input tensor
     const Tensor& input_tensor = context->input(0);
-    auto input = input_tensor.flat<int32>();
+    auto input = input_tensor.flat<float>();
     // Create an output tensor
     Tensor* output_tensor = NULL;
     OP_REQUIRES_OK(context, context->allocate_output(0, input_tensor.shape(),
                                                      &output_tensor));
-    auto output = output_tensor->flat<int32>();
+    auto output = output_tensor->flat<float>();
     const int N = input.size();
     for (int i = 1; i < N; i++) {
       output(i) = (float)i;
--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 import numpy as np
+import sys
 
-example_op_module = tf.load_op_library('libcpp_example.so')
+if sys.platform == 'darwin':
+    example_op_module = tf.load_op_library('libcpp_example.dylib')
+else:
+    example_op_module = tf.load_op_library('libcpp_example.so')
 with tf.Session():
-    print(example_op_module.example_op(np.zeros(101)).eval())
+    print(example_op_module.example_op(np.zeros(101, dtype=np.float32)).eval())
