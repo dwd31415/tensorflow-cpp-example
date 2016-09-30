4fcc6e2bd65cc238875d5208df38f2bc08382cb5
Adrian Dawid <adriandwd@gmail.com>
2016-09-30 21:39:11 +0200

    Force CPU in test.py

--- test.py
+++ test.py
@@ -6,5 +6,7 @@ if sys.platform == 'darwin':
     example_op_module = tf.load_op_library('libcpp_example.dylib')
 else:
     example_op_module = tf.load_op_library('libcpp_example.so')
-with tf.Session():
-    print(example_op_module.example_op(np.zeros(101, dtype=np.float32)).eval())
+
+with tf.device("/cpu:0"):
+    with tf.Session():
+        print(example_op_module.example_op(np.zeros(101, dtype=np.float32)).eval())
